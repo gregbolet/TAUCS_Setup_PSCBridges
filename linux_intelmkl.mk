0bb247229a7b837344848d28ef597067c7f74a20
Gregory Bolet <gbolet@fandm.edu>
2018-06-15 17:53:06 -0400

    Update linux_intelmkl.mk
    Got rid of unneeded comments

--- linux_intelmkl.mk
+++ linux_intelmkl.mk
@@ -14,14 +14,7 @@ FC        = ifort
 FFLAGS    = -O3
 FOUTFLG   = -o ./
 
-# -Xc: strict ANSI (Xa is extended, -c99 is C99)
-# -c99: some c99 features (-c99- to disallow)
-# -axW: generate Pentium4 optimized code, as well as generic 386
-# -axK: generate Pentium3 optimized code, as well as generic 386
-# -xK, -XW: generate Pentium? optimized code only
-# -vec_report0: do not report information on vectorization
-# -ansi_alias: assume no strange aliases (int to float, etc)
-# -fno-fnalias: no array aliasing within functions
+
 CC        = icc
 CFLAGS    = -O3 -Wall -Werror -std=c99
 
