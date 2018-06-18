9166375304b7b9a8bcba27845023aaec59417171
Gregory Bolet <gbolet@fandm.edu>
2018-06-17 20:07:44 -0400

    Simplified the Makefile

--- linux_intelmkl.mk
+++ linux_intelmkl.mk
@@ -10,14 +10,16 @@ F2CEXT=.f
 PATHSEP=/
 DEFFLG=-D
 
+METISROOT = /home/gbolet/Research/METIS/metis-4.0.3
+
 FC        = ifort
 FFLAGS    = -O3
 FOUTFLG   = -o ./
 
 CC        = icc
 CFLAGS    = -O3 -Wall -Werror -std=c99 \
-	    -I/home/gbolet/Research/METIS/metis-4.0.3/Lib \
-            -I/home/gbolet/Research/METIS/metis-4.0.3/Programs \
+	    -I${METISROOT}/Lib \
+            -I${METISROOT}/Programs
 
 
 COUTFLG   = -o ./
@@ -34,7 +36,7 @@ RM        = rm -rf
 
 LIBBLAS   = -mkl=parallel -lpthread
 LIBLAPACK =
-LIBMETIS  = /home/gbolet/Research/METIS/metis-4.0.3/libmetis.a
+LIBMETIS  = ${METISROOT}/libmetis.a
 
 #LIBMETIS  = /usr/lib64/libmetis.so.0 
 
